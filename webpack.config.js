f7f15ddcd5f95fe13311d7cd7805021d32d0b68f
Prakhar <prakharpalpfdr+github@gmail.com>
2021-11-28 14:43:33 +0530

    feat: add rounded corners to scrollbar and add padding to left and right of the scrollbar
    now it looks like Firefox's scrollbar on Linux

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(scss|css)$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            },
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({ filename: 'main.css',  })
+    ]
+}
